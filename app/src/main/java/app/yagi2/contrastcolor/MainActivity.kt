9f410d0845336805229a5aaae89bd86e8eec4368
Itsuki Aoyagi <mail@yagi2.dev>
2020-02-29 11:05:10 +0900

    DONE!!!!!!

--- app/src/main/java/app/yagi2/contrastcolor/MainActivity.kt
+++ app/src/main/java/app/yagi2/contrastcolor/MainActivity.kt
@@ -2,7 +2,6 @@ package app.yagi2.contrastcolor
 
 import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
